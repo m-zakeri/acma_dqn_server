5e67289911847e360ed9714fbc6abe5cd6c40e49
hrahmadi71 <h.ahmadi60@yahoo.com>
2019-12-29 13:04:58 +0330

    basic train and get_q_values APIs have developed

--- application/dqn.py
+++ application/dqn.py
@@ -1,8 +1,7 @@
-from enum import *
 import tensorflow as tf
-import numpy as np
 
-class DQN:
+
+class DQNCore:
     def __init__(self, learning_rate, discount):
         self.discount = discount
 
@@ -13,6 +12,7 @@ class DQN:
         self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
                            loss=tf.losses.Huber(),
                            metrics=[tf.metrics.Accuracy()])
+        self.model.summary()
 
     def __define_model(self):
         self.model = tf.keras.models.Sequential([
@@ -26,6 +26,12 @@ class DQN:
             tf.keras.layers.Dense(units=self.__output_count)
         ])
 
+    def get_input_size(self):
+        return self.__input_count
+
+    def get_output_size(self):
+        return self.__output_count
+
     def get_q_values(self, state):
         return self.model.predict(x=[state], batch_size=1)[0].tolist()
 
@@ -35,5 +41,21 @@ class DQN:
 # todo: old_state_q_values[action] = reward + self.discount * np.amax(new_state_q_values)
 
 
-class DQ:
-    network = DQN(learning_rate=0.01, discount=0.95)
+class DQN:
+    __network = DQNCore(learning_rate=0.01, discount=0.95)
+
+    @staticmethod
+    def get_q_values(state):
+        return DQN.__network.get_q_values(state)
+
+    @staticmethod
+    def train(state, q_values):
+        DQN.__network.train(state, q_values)
+
+    @staticmethod
+    def get_state_regular_len():
+        return DQN.__network.get_input_size()
+
+    @staticmethod
+    def get_q_values_list_regular_len():
+        return DQN.__network.get_output_size()
--- application/routes.py
+++ application/routes.py
@@ -1,41 +1,73 @@
 from application import app, api
 from flask import jsonify
 from flask_restplus import Resource, fields
-from application.dqn import DQ
+from application.dqn import DQN
+from werkzeug.exceptions import BadRequest
 
-# @app.route('/')
-# def hello():
-#     return 'Hello Hamid!'
 
-person_model = api.model('Person', {
-    'first-name': fields.String(required=True),
-    'last-name': fields.String(required=True)
+# Swagger models:
+
+get_q_values_request_body_model = api.model('DQNInput', {
+    'state': fields.List(fields.Float)
 })
 
-@api.route('/api/')
-class GetAndPost(Resource):
-    def get(self):
-        d = {'msg': 'this is a test!'}
-        return jsonify(d)
+train_model_request_body_model = api.model('DQNTrainModel', {
+    'state': fields.List(fields.Float),
+    'q_values': fields.List(fields.Float)
+})
 
-    @api.doc(body=person_model)
-    def post(self):
-        data = api.payload
-        dic = {
-            'fname': data['first-name'],
-            'lname': data['last-name']
-        }
-        return jsonify(dic)
 
+# error handling functions:
+def evaluate_state(state):
+    if len(state) != DQN.get_state_regular_len():
+        raise BadRequest("Size of 'state' tensor must be {0} but length of the state you sent is {1}!"
+                         .format(DQN.get_state_regular_len(), len(state)))
+
+    if not all(isinstance(e, float) or isinstance(e, int) for e in state):
+        raise BadRequest("Every element of 'state' must be integer or float")
+
+
+def evaluate_q_values(q_values):
+    if len(q_values) != DQN.get_q_values_list_regular_len():
+        raise BadRequest("Size of 'q_values' tensor must be {0} but length of the q_values you sent is {1}!"
+                         .format(DQN.get_q_values_list_regular_len(), len(q_values)))
+
+    if not all(isinstance(e, float) or isinstance(e, int) for e in q_values):
+        raise BadRequest("Every element of 'q_values' must be integer or float")
+
+
+# APIs:
 
 @api.route('/get-q-values/')
-class get_q_values(Resource):
+class GetQValues(Resource):
+    @api.doc(body=get_q_values_request_body_model)
     def post(self):
         data = api.payload
         state = data['state']
+        evaluate_state(state)
         dic = {
-            'q_values': DQ.network.get_q_values(state=state)
+            'q_values': DQN.get_q_values(state=state)
         }
         return jsonify(dic)
+
+
+@api.route('/train/')
+class TrainModel(Resource):
+    @api.doc(body=train_model_request_body_model)
+    def post(self):
+        data = api.payload
+        state = data['state']
+        q_values = data['q_values']
+        evaluate_state(state)
+        evaluate_q_values(q_values)
+        DQN.train(state=state, q_values=q_values)
+        return jsonify({'message': 'q_values updated successfully.'})
+
+
+
+
+
+
+
 
 
